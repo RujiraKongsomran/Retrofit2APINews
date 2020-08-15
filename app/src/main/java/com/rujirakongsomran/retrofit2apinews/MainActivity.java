55de5415e49afaabd32dc28a69b8d87f9ef03daa
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-08-15 23:35:26 +0700

    Call Service

--- app/src/main/java/com/rujirakongsomran/retrofit2apinews/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/retrofit2apinews/MainActivity.java
@@ -3,12 +3,49 @@ package com.rujirakongsomran.retrofit2apinews;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.rujirakongsomran.retrofit2apinews.Model.News;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initInstances();
+    }
+
+    private void initInstances() {
+        tvResult = (TextView) findViewById(R.id.tvResult);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://newsapi.org/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        Call<News> call = jsonPlaceHolderApi.getListNews();
+        call.enqueue(new Callback<News>() {
+            @Override
+            public void onResponse(Call<News> call, Response<News> response) {
+                if (!response.isSuccessful()) {
+                    tvResult.setText("Code: " + response.code());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<News> call, Throwable t) {
+                tvResult.setText("Code: " + t.getMessage());
+            }
+        });
     }
 }

248adbdef630d6f19d1a5c09ac812c44c4f81539
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-08-15 23:45:14 +0700

    Convert Object to JSON String and set format json

--- app/src/main/java/com/rujirakongsomran/retrofit2apinews/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/retrofit2apinews/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
 import com.rujirakongsomran.retrofit2apinews.Model.News;
 
 import retrofit2.Call;
@@ -42,6 +43,11 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 News news = response.body();
+
+                Gson gson = new Gson();
+                String json = gson.toJson(news);
+                String jsonFormat = formatString(json);
+                tvResult.setText(jsonFormat);
             }
 
             @Override
@@ -49,5 +55,37 @@ public class MainActivity extends AppCompatActivity {
                 tvResult.setText("Code: " + t.getMessage());
             }
         });
+    }
+
+    public static String formatString(String text){
+
+        StringBuilder json = new StringBuilder();
+        String indentString = "";
+
+        for (int i = 0; i < text.length(); i++) {
+            char letter = text.charAt(i);
+            switch (letter) {
+                case '{':
+                case '[':
+                    json.append("\n" + indentString + letter + "\n");
+                    indentString = indentString + "\t";
+                    json.append(indentString);
+                    break;
+                case '}':
+                case ']':
+                    indentString = indentString.replaceFirst("\t", "");
+                    json.append("\n" + indentString + letter);
+                    break;
+                case ',':
+                    json.append(letter + "\n" + indentString);
+                    break;
+
+                default:
+                    json.append(letter);
+                    break;
+            }
+        }
+
+        return json.toString();
     }
 }
